034b3afd91fb8267391dca2872394fb28f27be6d
Hazem Emad <rowan.mayman@hotmail.com>
2018-04-11 03:12:03 +0200

    login with firebase ui

---
+++ app/src/main/java/com/github/rowanmohamed/al3abyala/activity/LoginActivity.java
@@ -0,0 +1,69 @@
+package com.github.rowanmohamed.al3abyala.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.github.rowanmohamed.al3abyala.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private static final int RC_SIGN_IN = 100;
+    private static final String FIREBASE_TOS_URL = "https://firebase.google.com/terms/";
+    private static final String FIREBASE_PRIVACY_POLICY_URL = "https://firebase.google.com/terms/analytics/#7_privacy";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null){
+            login();
+        } else {
+            openHome();
+        }
+    }
+
+    private void openHome() {
+        // TODO: 4/11/2018 open Home activity
+    }
+
+    private void login() {
+        List<AuthUI.IdpConfig> selectedProviders = new ArrayList<>();
+        selectedProviders.add(new AuthUI.IdpConfig.EmailBuilder()
+                .setRequireName(true)
+                .setAllowNewAccounts(true)
+                .build());
+
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setTheme(R.style.AppTheme)
+                        .setLogo(R.mipmap.ic_launcher)
+                        .setAvailableProviders(selectedProviders)
+                        .setTosUrl(FIREBASE_TOS_URL)
+                        .setPrivacyPolicyUrl(FIREBASE_PRIVACY_POLICY_URL)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN && resultCode == Activity.RESULT_OK) {
+            openHome();
+        } else {
+            finish();
+        }
+    }
+}
